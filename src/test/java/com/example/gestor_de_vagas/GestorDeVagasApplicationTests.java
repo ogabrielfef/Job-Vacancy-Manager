b23fde57f3fb84b1e6fed42f782f96a891e0a4e6
ogabrielfef <ogabrielfef@gmail.com>
2024-11-22 22:38:14 -0300

    estrutura do projeto

---
+++ src/test/java/com/example/gestor_de_vagas/GestorDeVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestor_de_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
